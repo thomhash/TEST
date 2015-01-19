240326d4aaacc11c037d6093c4632f1b9e8dd51b
thomhash <thindberg@gmail.com>
2015-01-19 18:28:49 +0100

    thomas ændring

--- dl/login.php
+++ dl/login.php
@@ -4,5 +4,10 @@ $db_database = 'webshop';
 $db_username = 'root';
 $db_password = '';
 
+$db_server = new mysqli($db_hostname, $db_username, $db_password);
+$db_server->connect($db_hostname, $db_username, $db_password, $db_database);
+
+
+
 ?>
 
