e63cfc30174b9531931fab3eec6ca40838157f63
mikkelbra <mikkelbra@gmail.com>
2015-02-28 22:35:01 +0100

    Merge origin/master
    Conflicts:
    	dl/login_tjek.php
    	vl/kurv_oversigt.php

--- dl/login_tjek.php
+++ dl/login_tjek.php
@@ -17,3 +17,21 @@ AND `adgangskode` = '$kode';";
     // $logget = $row[1];
     return $tal;
 }
+
+function login_tjek_kunde($brugernavn,$kode)
+{
+require 'login.php';
+  
+    $sql = "SELECT `id_kunde`
+FROM `kunde`
+WHERE `email` = '$brugernavn'
+AND `adgangskode` = '$kode';";
+    
+    $result= mysqli_query($db_server, $sql);       
+    $row= mysqli_fetch_all($result);
+    // $count = mysql_num_rows($row);
+    $tal = count($row);
+    mysqli_close($db_server);
+    // $logget = $row[1];
+    return $tal;
+}
